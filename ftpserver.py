456e5bc303e0dabf130c1a16b73606b0f50d6481
heyeepc <2675458141@qq.com>
2026-05-07 17:39:20 +0800

    Update ftpserver.py

--- ftpserver.py
+++ ftpserver.py
@@ -3,6 +3,8 @@ from pyftpdlib.handlers import FTPHandler,ThrottledDTPHandler
 from pyftpdlib.log import LogFormatter
 import logging
 
+from pyftpdlib.servers import FTPServer
+
 logging = logging.getLogger('pyftpdlib')
 logging.setLevel(logging.INFO)
 ch = logging.StreamHandler()
@@ -13,3 +15,21 @@ fh.setFormatter(LogFormatter())
 logging.root.addHandler(ch)
 logging.root.addHandler(fh)
 
+authorizer = DummyAuthorizer()
+authorizer.add_user('user', '12345', 'd:/', perm='elero_admin')
+authorizer.add_anonymous('d:/')
+
+handler = FTPHandler
+handler.authorizer =authorizer
+
+handler.passive_ports = range(2000,2333)
+
+dtp_handler = ThrottledDTPHandler
+dtp_handler.read_limit = 300 * 1024
+dtp_handler.write_limit = 300 * 1024
+handler.dtp_handler = dtp_handler
+
+server = FTPServer(('0.0.0.0', 80), handler)
+server.max_cons =150
+server.max_cons_per_ip = 15
+server.serve_forever()
