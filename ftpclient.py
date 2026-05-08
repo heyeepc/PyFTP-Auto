872e1374fa1f0a59fa937678a76f099ce957a3a1
heyeepc <2675458141@qq.com>
2026-05-08 13:13:22 +0800

    Update ftpclient.py

--- ftpclient.py
+++ ftpclient.py
@@ -1,1 +1,10 @@
+from ftplib import FTP
 
+ftp = FTP(host='localhost',user='user',passwd='12345')
+ftp.encoding = 'gbk'
+ftp.cwd('test')
+ftp.retrlines('list')
+ftp.retrbinary('RETR test',open('note.txt','wb').write)
+ftp.storbinary('STOR ftpserver.py',open('ftpserver.py','rb'))
+for f in ftp.mlsd(path='/test'):
+    print(f)
